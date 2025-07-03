9ac1343a35d48590d90e7acf69a006533556098e
YanRomeroAr <yan.romeroar@gmail.com>
2025-07-03 05:05:44 -0500

    Update app.py

--- streamlitcloud_chatbot/app.py
+++ streamlitcloud_chatbot/app.py
@@ -17,7 +17,7 @@ with st.sidebar:
         'gpt-4'
     ), key="model")
 
-    image = Image.open('logos.png')
+    image = Image.open('foto.png')
     st.image(image, caption='OpenAI, Langchain y Streamlit')
 
     st.markdown("Integrando OpenAI con Streamlit y Langchain.")
